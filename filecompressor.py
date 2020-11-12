53e0f262c85a0dedca2e1f5106f37c8b4378a943
Alex <rzheusskij99@gmail.com>
2020-11-12 17:51:56 +0300

    add class FileCompressor

---
+++ filecompressor.py
@@ -0,0 +1,7 @@
+class FileCompressor():
+    def __init__(self,filepath = 'files/file',compressedpath = 'files/compressedFile'):
+        self.filepath = filepath
+        self.compressedpath = compressedpath
+
+    def create_dict(self):
+        pass
