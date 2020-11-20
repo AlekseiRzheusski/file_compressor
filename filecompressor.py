d0bb1144d5d50ce3784438daca97bf55efe313df
Alex <rzheusskij99@gmail.com>
2020-11-20 21:22:36 +0300

    fix create_compressed_file method

--- filecompressor.py
+++ filecompressor.py
@@ -14,7 +14,7 @@ class FileCompressor():
         self.compressedpath = compressedpath
 
     def create_utf8(self):
-        with io.open(self.filepath, 'r',encoding='utf-8') as file:
+        with io.open(self.filepath, 'r', encoding='utf-8') as file:
             tmp_string = file.read()
         with io.open(self.filepath, 'a', encoding='utf-8') as writed_file:
             writed_file.write(tmp_string)
@@ -22,10 +22,10 @@ class FileCompressor():
     def create_list_of_words(self):
         list_of_words = {}
         words = []
-        with io.open(self.filepath, 'r',encoding='utf-8') as file:
+        with io.open(self.filepath, 'r', encoding='utf-8') as file:
             for line in file.readlines():
                 words.clear()
-                translator = str.maketrans('','', string.punctuation)
+                translator = str.maketrans('', '', string.punctuation)
                 line = line.translate(translator)
                 line = line.lower()
                 words = line.split()
@@ -56,18 +56,38 @@ class FileCompressor():
         print(dictionary)
         key_line = '5'
         for key, value in dictionary.items():
-            key_line+=value+key
+            key_line += value + key
         with io.open(self.compressedpath, 'w', encoding='utf-8') as file:
             file.write(key_line)
-        with io.open(self.filepath, 'r') as readed_file:
+        with io.open(self.filepath, 'r', encoding='utf-8') as readed_file:
             with io.open(self.compressedpath, 'a', encoding='utf-8') as writed_file:
                 for line in readed_file.readlines():
                     words.clear()
                     words = line.split()
                     for i in range(len(words)):
-                        if words[i] in dictionary:
-                            words[i] = dictionary[words[i]]
+                        tmp_str = ""
+                        index = 1
+                        if words[i][0] in string.punctuation:
+                            index = 0
+                            tmp_str += words[i][0]
+                            translator = str.maketrans('', '', string.punctuation)
+                            words[i] = words[i].translate(translator)
+                        elif words[i][-1] in string.punctuation:
+                            tmp_str += words[i][-1]
+                            index = -1
+                            translator = str.maketrans('', '', string.punctuation)
+                            words[i] = words[i].translate(translator)
+
+                        if words[i].lower() in dictionary:
+                            if index == 0:
+                                key = dictionary[words[i].lower()]
+                                words[i] = tmp_str
+                                words[i] += key
+                            elif index == -1:
+                                words[i] = dictionary[words[i].lower()]
+                                words[i] += tmp_str
+                            else:
+                                words[i] = dictionary[words[i].lower()]
                     writed_line = ' '.join(words)
                     print(writed_line)
-                    writed_file.write(writed_line+'\n')
-
+                    writed_file.write(writed_line + '\n')
