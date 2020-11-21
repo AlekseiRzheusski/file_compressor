5549d9473dd6be126093159c42380e770aabee39
Alex <rzheusskij99@gmail.com>
2020-11-21 13:35:27 +0300

    create FileDecompressor class

---
+++ filedecompressor.py
@@ -0,0 +1,6 @@
+class FileDecompressor:
+    def __init__(self,compressedpath = 'files/compressedFile', decompressedpath = 'file/decompressedFile'):
+        self.compressedpath = compressedpath
+        self.decompressedpath =decompressedpath
+
+    
